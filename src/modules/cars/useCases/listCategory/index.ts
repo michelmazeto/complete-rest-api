f747ad9255dd9e220062871c16c88fe40df153e5
michelmazeto <michel.mazzetto@gmail.com>
2023-07-26 17:35:18 -0400

    refactor: move CategoriesRepository.ts and SpecificationsRepository.ts to implementations folder

--- src/modules/cars/useCases/listCategory/index.ts
+++ src/modules/cars/useCases/listCategory/index.ts
@@ -1,4 +1,4 @@
-import { CategoriesRepository } from '../../repositories/CategoriesRepository';
+import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository';
 import { ListCategoriesController } from '../listCategory/ListCategoriesController';
 import { ListCategoriesUseCase } from '../listCategory/ListCategoriesUseCase';
 
