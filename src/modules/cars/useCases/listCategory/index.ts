d522bf7408997c561ddbdff841613fcec2d767b3
michelmazeto <michel.mazzetto@gmail.com>
2023-07-26 17:31:35 -0400

    feat: add static getInstance method to CategoriesRepository

--- src/modules/cars/useCases/listCategory/index.ts
+++ src/modules/cars/useCases/listCategory/index.ts
@@ -2,7 +2,7 @@ import { CategoriesRepository } from '../../repositories/CategoriesRepository';
 import { ListCategoriesController } from '../listCategory/ListCategoriesController';
 import { ListCategoriesUseCase } from '../listCategory/ListCategoriesUseCase';
 
-const categoriesRepository = new CategoriesRepository();
+const categoriesRepository = CategoriesRepository.getInstance();
 
 const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository);
 
