5820e9cbb008caf211dc79470ac919ca71648390
michelmazeto <michel.mazzetto@gmail.com>
2023-08-03 16:12:30 -0400

    feat: added authenticate user and jwt

--- src/database/index.ts
+++ src/database/index.ts
@@ -7,7 +7,7 @@ import { User } from '../modules/accounts/entities/User';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
+  host: 'banco',
   port: 5432,
   username: 'postgres',
   password: 'postgres',
